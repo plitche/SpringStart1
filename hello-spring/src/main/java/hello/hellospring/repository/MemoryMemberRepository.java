fe4062c821fb7d13c185213f49e5acd0bb7b3625
plitche <kysu728@naver.com>
2022-03-04 00:01:24 +0900

    스프링 빈과 의존관계 - 컴포넌트 스캔과 자동 의존관계 설정
    컴포넌트 스캔과 자동 의존관계 설정

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     // 동시성 문제로 인해서 공유되는 변수일때는 ?? 맵?

228538b4c7f25e0038dcadfa6aec1d7ddb49fec2
plitche <kysu728@naver.com>
2022-03-05 01:22:40 +0900

    회원 웹 기능 - 등록

--- hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,7 +1,10 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -10,4 +13,10 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
 }
--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,10 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,6 +14,18 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }

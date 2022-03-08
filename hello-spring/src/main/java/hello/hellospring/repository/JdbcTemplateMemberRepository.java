03b80feef0c87ba93a0f107b3e1f1fd9557f04f9
plitche <kysu728@naver.com>
2022-03-09 00:57:30 +0900

    no message

--- hello-spring/src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -28,20 +28,24 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
         member.setId(key.longValue());
         return member;
     }
+
     @Override
     public Optional<Member> findById(Long id) {
         List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
         return result.stream().findAny();
     }
+
     @Override
     public List<Member> findAll() {
         return jdbcTemplate.query("select * from member", memberRowMapper());
     }
+
     @Override
     public Optional<Member> findByName(String name) {
         List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
         return result.stream().findAny();
     }
+    
     private RowMapper<Member> memberRowMapper() {
         return (rs, rowNum) -> {
             Member member = new Member();
